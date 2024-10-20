37c259182d6f0a9bda5e7345da1ab4f28774b856
Fl Fl <kevinzhou@berkeley.edu>
2024-10-20 04:16:00 -0700

    some updates

--- static/js/app.js
+++ static/js/app.js
@@ -1,83 +1,180 @@
-// Build the metadata panel
+/* Naming conventions:
+all- prefix is used for variables containing all the elements of a data type (names, metadata, samples).
+selected- prefix is used for the picked or desired element of an object or array.
+The d in Metadata is never capitalized. Similar treatment for other compound words, like dataset.
+The "sample" variable refers to the value of the study subject's id, which is listed in the names object.
+It does not refer to the samples array within the dataset.*/
+
+// Build the panel that displays the metadata of the currently selected sample subject
 function buildMetadata(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
+    
+    // 1. FUNCTION to FILTER for the DESIRED SAMPLE'S METADATA
+    // Assign a variable to the array of all metadata
+    let allMetadata = data.metadata; // allMetadata is an array of objects
 
-    // get the metadata field
+    // Filter the metadata for the object with the desired name (aka sample id)
+    function selectMetadata(object) {
+      return object.id === parseInt(sample);
+    }
+    let selectedMetadata = allMetadata.filter(selectMetadata)[0]; //selectedMetadata is an object
 
+    /* parseInt had to be used above because it was discovered that the values of 'id' keys within
+    the objects of the metadata array are numbers, while the "sample" from the DOM dropdown is a
+    string. Use of "==" instead of "===" would have also worked, to perform type coercion.*/
+    /*  console.log(`Data type of sample: ${typeof sample}`)
 
-    // Filter the metadata for the object with the desired sample number
+    for (let i = 0; i < Object.entries(selectedMetadata).length; i++) {
+      console.log(`Type: ${Object.keys(selectedMetadata)[i]}: ${Object.values(selectedMetadata)[i]} ${typeof Object.values(selectedMetadata)[i]}`);
+    }    */
 
+    // 2. PREPARE the DOM ELEMENT
+    // Select the panel with id `#sample-metadata`
+    let metadataPanel = d3.select("#sample-metadata");
 
-    // Use d3 to select the panel with id of `#sample-metadata`
+    // Use .html("") to clear existing value from the panel's DOM element
+    metadataPanel.html("");
 
+    // 3. APPEND the SELECTED SAMPLE'S METADATA to the DOM ELEMENT
+    let metadataPanelLabels = ["Study Subject ID", "Ethnicity", "Gender", "Age", "Location", "Belly Button Type (Innie/Outie)", "Wash Frequency"];
 
-    // Use `.html("") to clear any existing metadata
-
-
-    // Inside a loop, you will need to use d3 to append new
-    // tags for each key-value in the filtered metadata.
-
+    for (let i= 0; i < Object.entries(selectedMetadata).length; i++) {
+      let entry = `<strong>${metadataPanelLabels[i]}:</strong> ${Object.values(selectedMetadata)[i]}`;
+      metadataPanel
+        .append("span")
+        .html(entry)
+        .append("br");
+    }
   });
 }
 
-// function to build both charts
+// Function to build both charts
 function buildCharts(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
-    // Get the samples field
-
+    // Assign a variable to the array of all samples
+    let allSamples = data.samples // allSamples is an array of objects
 
     // Filter the samples for the object with the desired sample number
+    function selectSample(object) {
+      return object.id === sample; // No type conversion needed since sample array's "id" are strings
+    }
+    let selectedSample = allSamples.filter(selectSample)[0]; // selectedSample is an object
 
+    // Get the otu_ids and sample_values
+    let selected_otu_ids = selectedSample.otu_ids
+    let selected_sample_values = selectedSample.sample_values
 
-    // Get the otu_ids, otu_labels, and sample_values
+    // Build a Bubble Chart, i.e., scatter plot with option `mode: 'markers'`
+    let trace_bubble = {
+      x: selected_otu_ids,
+      y: selected_sample_values,
+      mode: "markers",
+      marker: {
+        size: selected_sample_values,
+        color: selected_otu_ids,
+        colorscale: "rainbow",
+        showscale: true
+      }
+    };
 
+    let data_bubble = [trace_bubble];
 
-    // Build a Bubble Chart
-
+    let layout_bubble = {
+      title: "Abundance of Bacterial Cultures by OTU ID",
+      xaxis: {
+        range: [0, Math.max(...selected_otu_ids) * 1.1] // Adjust axis range for better visualization across all samples
+      },
+      yaxis: {
+        range: [0, Math.max(...selected_sample_values) * 1.5]
+      }
+    };
+    console.log(Math.max(...selected_otu_ids))
 
     // Render the Bubble Chart
+    Plotly.newPlot("bubble", data_bubble, layout_bubble);
+    
+    // BEGIN CODE for BAR CHART
+    // Place only the first 10 OTU IDs and sample_values into an array to use for y-ticks/x-values
+    let top_otus = selected_otu_ids
+      .slice(0,10)
+      .map(function(id) {
+        return `OTU ${id}`;
+      })
+    ;
 
+    let top_sample_values = selected_sample_values.slice(0,10);
 
-    // For the Bar Chart, map the otu_ids to a list of strings for your yticks
+    // Build a horizontal bar chart (x and y are reversed from a standard vertical bar chart)
+    let trace_bar = {
+      y: top_otus,
+      x: top_sample_values,
+      type: "bar",
+      orientation: 'h'
+    };
 
+    let data_bar = [trace_bar];
 
-    // Build a Bar Chart
-    // Don't forget to slice and reverse the input data appropriately
-
+    let layout_bar = {
+      title: {
+        text: "Top 10 Bacteria Cultures Found",
+        pad: {
+          t: 10
+        }
+      },
+      yaxis: { 
+        title: 'Operational Taxonomic Unit ID',
+        standoff: 20
+      },
+      xaxis: { 
+        title: 'Bacterial Abundance'
+      },
+    };
 
     // Render the Bar Chart
-
+    Plotly.newPlot("bar", data_bar, layout_bar);
   });
 }
 
 // Function to run on page load
 function init() {
-  d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
+  d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json")
+    .then((data) => {
+      // Assign a variable to the array of all names (i.e., study subject IDs)
+      let allNames = data.names
 
-    // Get the names field
+      // Select the dropdown div with id of `#selDataset`
+      let dropdownMenu = d3.select("#selDataset");
 
+      // Loop through the array to add all names to the selected dropdown
+      for (let i = 0; i < allNames.length; i++) {
+        dropdownMenu
+          .append("option")
+          .text(allNames[i])
+          .attr("value", allNames[i]); // Still don't understand difference between text and attr...
+      }
 
-    // Use d3 to select the dropdown with id of `#selDataset`
+      // BUILD CHARTS and PANEL USING the FIRST SAMPLE
 
+      // Assign a variable to the name that appears first in the data
+      let inauguralName = allNames[0]
 
-    // Use the list of sample names to populate the select options
-    // Hint: Inside a loop, you will need to use d3 to append a new
-    // option for each sample name.
+      // Populate the panel
+      buildMetadata(inauguralName);
 
-
-    // Get the first sample from the list
-
-
-    // Build charts and metadata panel with the first sample
-
-  });
+      // Populate the charts
+      buildCharts(inauguralName);
+    })
+    .catch((error) => { // Runs a function if the promise fails
+      console.error("Error fetching the data during initialization:", error);
+    });
 }
 
 // Function for event listener
-function optionChanged(newSample) {
+function optionChanged(sample) {
   // Build charts and metadata panel each time a new sample is selected
-
+  buildMetadata(sample);
+  buildCharts(sample);
 }
 
 // Initialize the dashboard
